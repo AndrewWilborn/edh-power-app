0fe63990e528f9d983ace5b6002b8167defe5966
Andrew Wilborn <andrew.j.wilborn@sbcglobal.net>
2023-07-22 18:13:08 -0400

    ADDED ability to pull data from database

--- app/page.js
+++ app/page.js
@@ -1,6 +1,39 @@
+"use client"
+import { useEffect, useState } from "react"
+import Card from "./shared/card";
 
 export default function Home() {
+
+  const [decks, setDecks] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/decks")
+      .then(response => response.json())
+      .then(data => {
+        setDecks(data)
+        console.log(data[0].deck_name)
+      })
+      .catch(alert)
+  }, [setDecks])
+
   return (
-    <h1>Hello</h1>
+    <section className="text-gray-400 body-font bg-gray-900">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-wrap w-full mb-8">
+          <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">H1 Here</h1>
+            <div className="h-1 w-20 bg-yellow-500 rounded"></div>
+          </div>
+          <p className="lg:w-1/2 w-full leading-relaxed text-gray-400 text-opacity-90">Possible Description here</p>
+        </div>
+        {!decks
+          ? <p>Loading...</p>
+          : decks.map(deck =>
+            <Card deck={deck} />
+          )
+
+        }
+      </div>
+    </section>
   )
 }
---
+++ app/shared/card.js
@@ -0,0 +1,28 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
+export default function Card({deck}) {
+  
+  const [commander, setCommander] = useState()
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/cards/${deck.commander}`)
+      .then(response => response.json())
+      .then(data => setCommander(data))
+      .catch(alert)
+  }, [setCommander])
+
+  return (
+    <div className="flex flex-wrap -m-4">
+    <div className="xl:w-1/4 md:w-1/2 p-4">
+      <div className="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
+        <img className="h-40 rounded w-full object-cover object-center mb-6" src={commander ? commander.image_uri : ""} alt="content"/>
+          <h2 className="text-lg text-white font-medium title-font mb-4">{deck.deck_name}</h2>
+          <h3 className="tracking-widest text-yellow-400 text-xs font-medium title-font">{commander ? commander.name : ""}</h3>
+          <p className="leading-relaxed text-base">P Tag</p>
+      </div>
+    </div>
+  </div>
+  )
+}
