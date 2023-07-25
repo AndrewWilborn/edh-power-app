d73999c83dbd79e1806ea135f24f2338bcc670ce
Andrew Wilborn <andrew.j.wilborn@sbcglobal.net>
2023-07-25 18:59:02 -0400

    ADDED myDecks page

--- app/myDecks/page.js
+++ app/myDecks/page.js
@@ -1,16 +1,25 @@
+
 "use client"
 import { useContext, useEffect, useState } from "react"
-import Card from "./shared/Card";
+import Card from "../shared/Card";
 import Link from "next/link";
 import { AuthContext } from "@/context/AuthContext";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+  const router = useRouter();
 
   const [decks, setDecks] = useState([]);
 
   const {user} = useContext(AuthContext)
 
+  if(!user){
+    router.push("/")
+  }
+
   useEffect(() => {
+    // TODO: get decks by user id
     fetch("http://localhost:3000/decks")
       .then(response => response.json())
       .then(data => {
@@ -24,15 +33,12 @@ export default function Home() {
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-wrap w-full mb-8">
           <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
-            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">All Decks</h1>
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">My Decks</h1>
             <div className="h-1 w-20 bg-orange-500 rounded"></div>
           </div>
-          {
-            user &&
-            <Link href="/myDecks" type="button" class="py-2 px-4  bg-orange-600 hover:bg-orange-700 focus:ring-orange-500 focus:ring-offset-orange-200 text-white transition mx-auto p-10 ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
-              View My Decks
+            <Link href="/createDeck" type="button" class="py-2 px-4  bg-orange-600 hover:bg-orange-700 focus:ring-orange-500 focus:ring-offset-orange-200 text-white transition mx-auto p-10 ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+              + New Deck
             </Link>
-          }
         </div>
         {!decks
           ? <p>Loading...</p>
