1882e0546c19426a5755cc21e4490a372184c2e9
Andrew Wilborn <andrew.j.wilborn@sbcglobal.net>
2023-07-28 19:12:30 -0400

    ADDED basic qr code functionality

--- app/shared/card.js
+++ app/shared/card.js
@@ -1,13 +1,17 @@
 "use client"
 
-import Link from "next/link"
 import { useEffect, useState } from "react"
+import { useQRCode } from "next-qrcode"
 
 export default function Card({ deck }) {
 
   console.log(deck)
 
   const [commander, setCommander] = useState()
+
+  const [showQR, setShowQR] = useState(false)
+
+  const { Canvas } = useQRCode()
 
   useEffect(() => {
     fetch(`http://localhost:3000/cards/${deck.commander}`)
@@ -16,13 +20,17 @@ export default function Card({ deck }) {
       .catch(alert)
   }, [setCommander])
 
+  const hanldeQR = () => {
+    setShowQR(true)
+  }
+
   const h3Style = "tracking-widest text-orange-400 text-xs font-medium title-font mb-1";
 
   return (
     <div className="xl:w-1/3 md:w-1/2 w-full p-4">
       <div className="bg-gray-800 bg-opacity-40 rounded-lg">
         {/* <img className="w-full rounded object-cover object-center mb-6" src={commander ? commander.image_uri : ""} alt="content" /> */}
-        <div className={`w-full aspect-2/1 bg-cover rounded-lg flex flex-col items-end`} style={{ backgroundImage: `url('${commander ? commander.image_uri : ""})`}}>
+        <div className={`w-full aspect-2/1 bg-cover rounded-lg flex flex-col items-end`} style={{ backgroundImage: `url('${commander ? commander.image_uri : ""})` }}>
           <p>Change Art</p>
           <p className="">Artist Name</p>
         </div>
@@ -36,6 +44,21 @@ export default function Card({ deck }) {
           <h3 className={h3Style}>{deck.num_ratings} Ratings</h3>
           {deck.decklist_url && <h3 className={h3Style}>{deck.decklist_url}</h3>}
         </div>
+        {showQR
+          ? <Canvas
+            text={'https://github.com/bunlong/next-qrcode'}
+            options={{
+              level: 'M',
+              margin: 3,
+              scale: 4,
+              width: 200,
+              color: {
+                dark: '#010599FF',
+                light: '#FFBF60FF',
+              },
+            }} />
+          : <button onClick={hanldeQR} className="btn btn-blue">Placeholder QR Code Button</button>
+        }
       </div>
     </div>
   )
