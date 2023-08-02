8e5a8c25eb7addd74db5f19ac61c839b01c88e6d
Andrew Wilborn <andrew.j.wilborn@sbcglobal.net>
2023-08-02 16:59:00 -0400

    ADDED wip screen to edit page

--- app/edit/[deckid]/page.js
+++ app/edit/[deckid]/page.js
@@ -1,7 +1,16 @@
+import Link from "next/link";
 
-export default function editDeck(){
+export default function editDeck() {
 
-  return(
-    <></>
+  return (
+    <section className="text-gray-400 bg-gray-900 body-font relative">
+      <div className="container px-5 py-24 mx-auto">
+        <h2 className="text-lg">WIP</h2>
+        <h3 className="text-sm">Deck Editor is currently in development</h3>
+        <br/>
+        <br/>
+        <Link href={"/"}>Click here to return to home</Link>
+      </div>
+    </section>
   )
 }
