cf5fa068619d0e98eecc8d970bbb27306cb5cb75
Andrew Wilborn <andrew.j.wilborn@sbcglobal.net>
2023-08-03 14:52:07 -0400

    MOVED decklist button to top left of image

--- app/shared/card.js
+++ app/shared/card.js
@@ -37,9 +37,12 @@ export default function Card({ deck, handleQR }) {
     <div className="xl:w-1/3 md:w-1/2 w-full px-4 my-2">
       <div className="bg-gray-800 bg-opacity-40 rounded-lg">
         <div className={`w-full aspect-16/7 bg-cover rounded-lg flex flex-col items-end justify-between`} style={{ backgroundImage: `radial-gradient(transparent, #00000088), url('${commander ? commander.image_uri : ""})` }}>
-          <button onClick={() => { toggleArt() }}>
-            <img src="/images/image-regular.svg" className="h-6 m-1" />
-          </button>
+          <div className="flex justify-between w-full">
+            {deck.decklist_url ? <a href={deck.decklist_url} className="h-6 m-1"><p className="text-gray-300 text-xs">View Decklist</p></a> : <div />}
+            <button onClick={() => { toggleArt() }}>
+              <img src="/images/image-regular.svg" className="h-6 m-1" />
+            </button>
+          </div>
           <div className="flex justify-between w-full">
             <h2 className="text-lg text-white font-medium title-font px-1">{deck.deck_name}</h2>
             <p className="h-auto mt-auto text-xs text-gray-300 px-1">{commander && commander.artist}</p>
@@ -50,27 +53,26 @@ export default function Card({ deck, handleQR }) {
           {partner && <h3 className={h3Style}>And: {partner.name}</h3>}
           <Ratingbar rating={deck.avg_rating} />
           <h3 className={h3Style}>{deck.num_ratings} Rating{deck.num_ratings !== 1 && 's'}</h3>
-          {deck.decklist_url && <h3 className={h3Style}>{deck.decklist_url}</h3>}
           {
             deck.owner &&
-              <>
-                {showDropdown &&
-                  <div className="flex w-full justify-around">
-                    <button className="flex flex-col"
-                      onClick={() => { handleQR(`http://localhost:3000/rate/${deck.id}`) }}>
-                      <img src="/images/qrcode-solid.svg" className="h-6 m-1" />
-                      <p>Get Ratings</p>
-                    </button>
-                    <Link className="flex flex-col"
-                      href={`/edit/${deck.id}`}>
-                      <img src="/images/pen-svgrepo-com.svg" className="h-6 m-1" />
-                      <p>Edit</p>
-                    </Link>
-                  </div>
-                }
-                <button className="mx-auto w-full"
-                  onClick={() => { setShowDropdown(!showDropdown) }}>Show Dropdown</button>
-              </>
+            <>
+              {showDropdown &&
+                <div className="flex w-full justify-around">
+                  <button className="flex flex-col"
+                    onClick={() => { handleQR(`http://localhost:3000/rate/${deck.id}`) }}>
+                    <img src="/images/qrcode-solid.svg" className="h-6 m-1" />
+                    <p>Get Ratings</p>
+                  </button>
+                  <Link className="flex flex-col"
+                    href={`/edit/${deck.id}`}>
+                    <img src="/images/pen-svgrepo-com.svg" className="h-6 m-1" />
+                    <p>Edit</p>
+                  </Link>
+                </div>
+              }
+              <button className="mx-auto w-full"
+                onClick={() => { setShowDropdown(!showDropdown) }}>Show Dropdown</button>
+            </>
           }
         </div>
       </div>
